e7588ac29b94d09185c17ee7448bb5e82e4df99c
Anwesh43 <anweshthecool0@gmail.com>
2021-04-29 00:50:13 +0530

    adding RingAroundBalLView class

--- ringaroundballview/src/main/java/com/example/ringaroundballview/RingAroundBallView.kt
+++ ringaroundballview/src/main/java/com/example/ringaroundballview/RingAroundBallView.kt
@@ -68,3 +68,18 @@ fun Canvas.drawRABNode(i : Int, scale : Float, paint : Paint) {
     drawRingAroundBall(scale, w, h, paint)
 }
 
+class RingAroundBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
