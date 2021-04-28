0696d6f1b9959fa8fa143ed6e9d04792c53a3b5a
Anwesh43 <anweshthecool0@gmail.com>
2021-04-29 00:38:26 +0530

    adding constants and libraries

--- ringaroundballview/src/main/java/com/example/ringaroundballview/RingAroundBallView.kt
+++ ringaroundballview/src/main/java/com/example/ringaroundballview/RingAroundBallView.kt
@@ -1,1 +1,28 @@
 package com.example.ringaroundballview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "",
+    "",
+    "",
+    "",
+    ""
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val deg : Float = 360f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
