8895b5b2f2a4cef638bd9031e0072920498d2c81
Anwesh43 <anweshthecool0@gmail.com>
2021-04-29 01:13:23 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedringaroundballview/MainActivity.kt
+++ app/src/main/java/com/example/linkedringaroundballview/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.linkedringaroundballview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.ringaroundballview.RingAroundBallView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RingAroundBallView.create(this)
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
